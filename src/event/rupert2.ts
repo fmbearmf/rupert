520b0ffe5c283da7558645369f3ec5ec0f392971
bearfm <77757734+bearfm@users.noreply.github.com>
2024-02-03 20:24:11 -0800

    rupert

--- src/event/rupert2.ts
+++ src/event/rupert2.ts
@@ -6,9 +6,8 @@ export default (client: Client): void => {
         if (message.author.id === client.user?.id)
             if (Math.random() < 0.98) return;
 
-        if (Math.random() >= 0.90) {
-            message.reply({ content: "dumb idiot" });
-            message.member?.setNickname("dumb idiot", "felt like it").catch(console.log);
+        if (Math.random() >= 0.99) {
+            message.reply({ content: "ok thats crazy" });
             return;
         }
 
