df16146f3e19da8eeb6d67c923af40766ab62cdc
bearfm <77757734+bearfm@users.noreply.github.com>
2024-02-04 23:20:13 -0800

    yeah

--- build/cmd/ExecCommand.js
+++ build/cmd/ExecCommand.js
@@ -23,17 +23,19 @@ exports.ExecCommand = {
             return;
         if (commandString.toLowerCase().includes("sudo ") || commandString.toLowerCase().includes("rm ") || commandString.toLowerCase().includes("systemctl ") || commandString.toLowerCase().includes("|")) {
             interaction.editReply("you can't run that command.");
+            return;
         }
-        (0, child_process_1.exec)(`sudo -u ubuntu /bin/bash -c "${commandString}" | sed 's/\x1B\\[[0-9;]\\{1,\\}[A-Za-z]//g'`, { shell: "/usr/bin/bash", env: { PATH: "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin" } }, (err, stdout, stderr) => {
-            if (err) {
-                interaction.editReply(stderr.toString()).catch(console.log);
-                return;
-            }
-            if (!stdout.toString()) {
-                interaction.editReply("** **");
-                return;
-            }
-            interaction.editReply(stdout.toString()).catch(console.log);
-        });
+        else
+            (0, child_process_1.exec)(`sudo -u ubuntu /bin/bash -c "${commandString}" | sed 's/\x1B\\[[0-9;]\\{1,\\}[A-Za-z]//g'`, { shell: "/usr/bin/bash", env: { PATH: "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin" } }, (err, stdout, stderr) => {
+                if (err) {
+                    interaction.editReply(stderr.toString()).catch(console.log);
+                    return;
+                }
+                if (!stdout.toString()) {
+                    interaction.editReply("** **");
+                    return;
+                }
+                interaction.editReply(stdout.toString()).catch(console.log);
+            });
     }
 };
