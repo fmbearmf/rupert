f4d36d00f49af69be17bdb9cb4fd78cb7063afb7
bearfm <77757734+bearfm@users.noreply.github.com>
2024-02-04 23:20:05 -0800

    yeah

--- src/cmd/ExecCommand.ts
+++ src/cmd/ExecCommand.ts
@@ -24,9 +24,7 @@ export const ExecCommand: Command = {
         if (commandString.toLowerCase().includes("sudo ") || commandString.toLowerCase().includes("rm ") || commandString.toLowerCase().includes("systemctl ") || commandString.toLowerCase().includes("|")) {
             interaction.editReply("you can't run that command.");
             return;
-        }
-        
-        exec(`sudo -u ubuntu /bin/bash -c "${commandString}" | sed 's/\x1B\\[[0-9;]\\{1,\\}[A-Za-z]//g'`, { shell: "/usr/bin/bash", env: { PATH: "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin" } }, (err, stdout, stderr) => {
+        } else exec(`sudo -u ubuntu /bin/bash -c "${commandString}" | sed 's/\x1B\\[[0-9;]\\{1,\\}[A-Za-z]//g'`, { shell: "/usr/bin/bash", env: { PATH: "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin" } }, (err, stdout, stderr) => {
             if (err) {
                 interaction.editReply(stderr.toString()).catch(console.log);
                 return;
