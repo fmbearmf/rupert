3b0d0dad1e6988fea8994174b2205c5a43e9ccbd
bear.fm <77757734+bearfm@users.noreply.github.com>
2024-02-05 17:40:07 -0800

    Update ExecCommand.ts

--- src/cmd/ExecCommand.ts
+++ src/cmd/ExecCommand.ts
@@ -21,7 +21,15 @@ export const ExecCommand: Command = {
         const commandString = commandObj.value?.toString();
         if (!commandString) return;
 
-        if (commandString.toLowerCase().includes("sudo ") || commandString.toLowerCase().includes("rm ") || commandString.toLowerCase().includes("systemctl ") || commandString.toLowerCase().includes("|") || commandString.toLowerCase().includes(">") || commandString.toLowerCase().includes("ln ") || commandString.toLowerCase().includes("&") || commandString.toLowerCase().includes("cat ")) {
+        if (commandString.toLowerCase().includes("sudo ") ||
+            commandString.toLowerCase().includes("rm ") ||
+            commandString.toLowerCase().includes("systemctl ") ||
+            commandString.toLowerCase().includes("|") ||
+            commandString.toLowerCase().includes(">") ||
+            commandString.toLowerCase().includes("ln ") ||
+            commandString.toLowerCase().includes("&") ||
+            commandString.toLowerCase().includes("cat ") ||
+            commandString.toLowerCase().includes("chmod ")) {
             interaction.editReply("you can't run that command.");
             return;
         } else exec(`sudo -u ubuntu /bin/bash -c "${commandString}" | sed 's/\x1B\\[[0-9;]\\{1,\\}[A-Za-z]//g'`, { shell: "/usr/bin/bash", env: { PATH: "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin" } }, (err, stdout, stderr) => {
