4601bbb3d6f9ea1a9d94d0aa53edc37ba25f63ca
bear.fm <77757734+bearfm@users.noreply.github.com>
2024-02-07 07:48:55 -0800

    Update DecideCommand.ts

---
+++ src/cmd/DecideCommand.ts
@@ -0,0 +1,22 @@
+import { ApplicationCommandType, Client, CommandInteraction } from "discord.js";
+import { Command } from "./cmd";
+import { eep } from "../eep";
+
+export const DecideCommand: Command = {
+    name: "decide",
+    description: "yes or no",
+    type: ApplicationCommandType.ChatInput,
+    run: async (client: Client, interaction: CommandInteraction) => {
+        interaction.deferReply();
+
+        let decision = true;
+
+        if (Math.random() <= 0.5) decision = false;
+
+        await eep(5000).then(() => {
+            interaction.editReply({
+                content: `${decision}`
+            });
+        });
+    }
+};
