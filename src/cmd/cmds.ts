9dafc380421cbc411811575b4ce424387d9ed193
bear.fm <77757734+bearfm@users.noreply.github.com>
2024-05-15 11:36:00 -0700

    Update cmds.ts

--- src/cmd/cmds.ts
+++ src/cmd/cmds.ts
@@ -1,12 +1,10 @@
 import { Command } from "./cmd";
-import { ExecCommand } from "./ExecCommand";
 import { RupertCommand } from "./RupertCommand";
 import { DecideCommand } from "./DecideCommand";
 import { YapCommand } from "./YapCommand";
 
 export const cmds: Array<Command> = [
     RupertCommand,
-    ExecCommand,
     DecideCommand,
     YapCommand
 ];
