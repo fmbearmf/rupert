d471d8ac25ae87e3bdfa6c4cefa5d17fe6435bc7
bear <77757734+fmbearmf@users.noreply.github.com>
2024-12-02 12:18:24 -0800

    Update RandomStatement.ts

--- src/event/ai/RandomStatement.ts
+++ src/event/ai/RandomStatement.ts
@@ -1,14 +1,17 @@
 import Adjectives from "./Adjectives";
 import Nouns from "./Nouns";
+import Verbs from "./Verbs";
 
 export default async (): Promise<string> => {
     const adjectives = Adjectives();
     const nouns = Nouns();
+    const verbs = Verbs();
 
     const nounIndex: number = Math.floor(Math.random() * nouns.length);
     const adjIndex: number = Math.floor(Math.random() * adjectives.length);
+    const verbIndex: number = Math.floor(Math.random() * verbs.length);
 
-    const stmt = `${adjectives[adjIndex]} ${nouns[nounIndex]}...`;
+    const stmt = `${adjectives[adjIndex]} ${nouns[nounIndex]} ${verbs[verbIndex]}...`;
 
     return stmt;
 };
