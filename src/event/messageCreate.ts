a77927a4161ee774db8a5bb032c6b3c8c34df9f8
bear <77757734+fmbearmf@users.noreply.github.com>
2024-12-07 18:37:38 -0800

    Update messageCreate.ts

--- src/event/messageCreate.ts
+++ src/event/messageCreate.ts
@@ -7,12 +7,12 @@ export default (client: Client): void => {
             if (Math.random() < 0.98) return;
 
         if (message.content.toLowerCase().includes("rupert")) {
-            message.reply({ content: "you called?" });
+            message.reply({ content: "bluepert." });
             return;
         }
 
         if (message.author.id === "470010465486110730") { // pixel's ID
-            if (Math.random() > 0.3) {
+            if (Math.random() >= 0.6) {
                 RandomStatement().then((m0) => {
                     RandomStatement().then((m1) => {
                         RandomStatement().then((m2) => {
@@ -23,7 +23,7 @@ export default (client: Client): void => {
             }
         }
 
-        if (Math.random() > 0.98) {
+        if (Math.random() >= 0.978) {
             RandomStatement().then((stmt) => {
                 message.reply(`${stmt}`);
             });
